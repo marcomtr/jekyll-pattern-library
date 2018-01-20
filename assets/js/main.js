c8d6e2a3bc38107dc10b1624fa48529fcdb5fd64
Marco <marco.mtr@gmail.com>
2018-01-20 14:33:05 +0000

    Add sidebar and javascript module

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+var patternLibrary = {
+
+	classes: {
+		section: ".js-section",
+		nav: ".js-style-guide-nav",
+		sourceTrigger: ".js-source-trigger",
+		sourceContainer: ".js-source",
+		menuLink: ".pl-sidebar-nav__link"
+	},
+
+	init: function() {
+		patternLibraryClasses = this.classes;
+		this.bindUIActions();
+
+	},
+
+
+	bindUIActions: function() {
+
+		$(patternLibraryClasses.menuLink).on('click', function(e) {
+			e.preventDefault();
+			$(this).next('.pl-sidebar-sub-nav').toggleClass('pl-sidebar-sub-nav--is-active');
+			
+		});
+
+		$(patternLibraryClasses.sourceTrigger).on('click', function(e) {
+			e.preventDefault();
+			$(this).parent().find(patternLibraryClasses.sourceContainer).toggleClass('u-hide');
+		});
+
+	}
+};
+
+patternLibrary.init();
